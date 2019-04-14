ab85b5c8fde8c39741a44ed9cdb956056b24833d
Edgar Santiago <itt.edgarstgo@gmail.com>
2019-04-13 21:39:09 -0400

    github pages initial

---
+++ precache-manifest.fdd97bc9bbf557e394242358bb795035.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e5afa24d3325f2d60e3d",
+    "url": "/static/js/main.3d957b3d.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "e5afa24d3325f2d60e3d",
+    "url": "/static/css/main.a151b1b4.chunk.css"
+  },
+  {
+    "revision": "545e5e27358fbd569540599f58c595a4",
+    "url": "/index.html"
+  }
+];
